906da6ecef1ea22e478c13e604de30aadd35e5b4
TienjinHuang <t.huang@tue.nl>
2022-07-02 18:02:40 +0200

    Add files via upload

--- T-GCN/T-GCN-PyTorch/tasks/supervised.py
+++ T-GCN/T-GCN-PyTorch/tasks/supervised.py
@@ -40,6 +40,12 @@ class SupervisedForecastTask(pl.LightningModule):
         batch_size, _, num_nodes = x.size()
         # (batch_size, num_nodes, hidden_dim)
         hidden = self.model(x)
+        hidden_mean,hidden_var=torch.chunk(hidden,chunks=2,dim=-1)
+        print("mean",hidden_mean.mean())
+        print("var",hidden_var.mean())
+        rand=torch.rand_like(hidden_var)
+        hidden=hidden_mean+(torch.sqrt(hidden_var+1e-10)*rand)
+        print("hidden",hidden.mean())
         # (batch_size * num_nodes, hidden_dim)
         hidden = hidden.reshape((-1, hidden.size(2)))
         # (batch_size * num_nodes, pre_len)
@@ -49,7 +55,31 @@ class SupervisedForecastTask(pl.LightningModule):
             predictions = hidden
         predictions = predictions.reshape((batch_size, num_nodes, -1))
         return predictions
-
+    def forward_val(self, x):
+        # (batch_size, seq_len, num_nodes)
+        batch_size, _, num_nodes = x.size()
+        # (batch_size, num_nodes, hidden_dim)
+        hidden = self.model(x)
+        hidden_mean,hidden_var=torch.chunk(hidden,chunks=2,dim=-1)
+        #rand=torch.rand_like(hidden_var)
+        hidden=hidden_mean
+        # (batch_size * num_nodes, hidden_dim)
+        hidden = hidden.reshape((-1, hidden.size(2)))
+        # (batch_size * num_nodes, pre_len)
+        if self.regressor is not None:
+            predictions = self.regressor(hidden)
+        else:
+            predictions = hidden
+        predictions = predictions.reshape((batch_size, num_nodes, -1))
+        return predictions
+    def val_step(self, batch, batch_idx):
+        # (batch_size, seq_len/pre_len, num_nodes)
+        x, y = batch
+        num_nodes = x.size(2)
+        predictions = self.forward_val(x)
+        predictions = predictions.transpose(1, 2).reshape((-1, num_nodes))
+        y = y.reshape((-1, y.size(2)))
+        return predictions, y
     def shared_step(self, batch, batch_idx):
         # (batch_size, seq_len/pre_len, num_nodes)
         x, y = batch
@@ -61,7 +91,12 @@ class SupervisedForecastTask(pl.LightningModule):
 
     def loss(self, inputs, targets):
         if self._loss == "mse":
-            return F.mse_loss(inputs, targets)
+            mean=self.model.tgcn_cell.mean
+            var=self.model.tgcn_cell.var
+            kl_loss=torch.mean(torch.square(mean)+var-torch.log(1e-10+var)-1,-1)*0.5
+            kl_loss=torch.sum(kl_loss)
+            print("kl loss",kl_loss.mean())
+            return F.mse_loss(inputs, targets)+self.hparams.kl_gamma*kl_loss
         if self._loss == "mse_with_regularizer":
             return utils.losses.mse_with_regularizer_loss(inputs, targets, self)
         raise NameError("Loss not supported:", self._loss)
@@ -73,7 +108,7 @@ class SupervisedForecastTask(pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        predictions, y = self.shared_step(batch, batch_idx)
+        predictions, y = self.val_step(batch, batch_idx)
         predictions = predictions * self.feat_max_val
         y = y * self.feat_max_val
         loss = self.loss(predictions, y)
@@ -107,6 +142,8 @@ class SupervisedForecastTask(pl.LightningModule):
     def add_task_specific_arguments(parent_parser):
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
         parser.add_argument("--learning_rate", "--lr", type=float, default=1e-3)
-        parser.add_argument("--weight_decay", "--wd", type=float, default=1.5e-3)
+        parser.add_argument("--weight_decay", "--wd", type=float, default=5e-4)
         parser.add_argument("--loss", type=str, default="mse")
+        parser.add_argument("--lamda", type=float, default=1.0)
+        parser.add_argument("--kl_gamma", type=float, default=5e-4)
         return parser
